dc5a5222be96b3a019a377c6353726f9840bf57d
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-07-15 18:35:43 +0300

    chore: project bootstrap

---
+++ src/test/java/app/hackoholics/api/TestApiApplication.java
@@ -0,0 +1,11 @@
+package app.hackoholics.api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
