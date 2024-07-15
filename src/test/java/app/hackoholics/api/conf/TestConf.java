8866133e68bac261216b1383ed3d44c876382f86
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-07-15 20:23:26 +0300

    feat: check api heath

---
+++ src/test/java/app/hackoholics/api/conf/TestConf.java
@@ -0,0 +1,28 @@
+package app.hackoholics.api.conf;
+
+import app.hackoholics.api.endpoint.rest.client.ApiClient;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class TestConf {
+
+  public static ApiClient anApiClient(String token, int serverPort) {
+    ApiClient client = new ApiClient();
+    client.setScheme("http");
+    client.setHost("localhost");
+    client.setPort(serverPort);
+    client.setRequestInterceptor(
+        httpRequestBuilder -> httpRequestBuilder.header("Authorization", "Bearer " + token));
+    return client;
+  }
+
+  public static int anAvailablePort() {
+    try {
+      return new ServerSocket(0).getLocalPort();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
---
+++ src/test/java/app/hackoholics/api/integration/HealthControllerIT.java
@@ -0,0 +1,52 @@
+package app.hackoholics.api.integration;
+
+import app.hackoholics.api.AbstractContextInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static app.hackoholics.api.conf.TestConf.anAvailablePort;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Testcontainers
+@ContextConfiguration(initializers = HealthControllerIT.ContextInitializer.class)
+@AutoConfigureMockMvc
+public class HealthControllerIT {
+  static class ContextInitializer extends AbstractContextInitializer {
+    public static final int SERVER_PORT = anAvailablePort();
+
+    @Override
+    public int getServerPort() {
+      return SERVER_PORT;
+    }
+  }
+
+  @Test
+  void ping_ok() throws IOException, InterruptedException {
+    // /!\ The HttpClient produced by openapi-generator SEEMS to not support text/plain
+    HttpClient unauthenticatedClient = HttpClient.newBuilder().build();
+    String basePath = "http://localhost:" + HealthControllerIT.ContextInitializer.SERVER_PORT;
+
+    HttpResponse<String> response =
+        unauthenticatedClient.send(
+            HttpRequest.newBuilder()
+                .uri(URI.create(basePath + "/ping"))
+                .header("Access-Control-Request-Method", "GET")
+                .build(),
+            HttpResponse.BodyHandlers.ofString());
+
+    assertEquals(HttpStatus.OK.value(), response.statusCode());
+    assertEquals("pong", response.body());
+  }
+}
