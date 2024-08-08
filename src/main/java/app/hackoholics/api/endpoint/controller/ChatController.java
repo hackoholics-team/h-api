3086ad0c135f33f445be90996ee477c166d418a2
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-08-08 20:47:32 +0300

    chore: call instance of ai model only one time

--- src/main/java/app/hackoholics/api/endpoint/controller/ChatController.java
+++ src/main/java/app/hackoholics/api/endpoint/controller/ChatController.java
@@ -2,15 +2,13 @@ package app.hackoholics.api.endpoint.controller;
 
 import app.hackoholics.api.service.ChatService;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
 public class ChatController {
-  private final ChatService service;
+  private final ChatService service = ChatService.getInstance();
 
   @PostMapping("/chat")
   public List<String> sendMessage(@RequestBody String message) {
--- src/main/java/app/hackoholics/api/service/ChatService.java
+++ src/main/java/app/hackoholics/api/service/ChatService.java
@@ -3,14 +3,24 @@ package app.hackoholics.api.service;
 import app.hackoholics.api.service.google.gemini.GeminiService;
 import com.google.cloud.vertexai.api.Part;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class ChatService {
-  private final GeminiService geminiService;
+  private static ChatService INSTANCE;
+  private static GeminiService geminiService;
+
+  private ChatService(GeminiService geminiService) {
+    ChatService.geminiService = geminiService;
+  }
+
+  public static ChatService getInstance() {
+    if (INSTANCE == null) {
+      return new ChatService(geminiService);
+    }
+    return INSTANCE;
+  }
 
   @SneakyThrows
   public List<String> sendMessage(String message) {
